a2f54ff2f1aa1267f2ba522e02da9d5fb8021491
Angelo <angelo.gonzalez@live.com>
2022-09-13 14:37:31 -0300

    310. Punto central, zoom y accessToken

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-full-screen',
@@ -19,13 +18,12 @@ export class FullScreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
-    (mapboxgl as any).accessToken = environment.mapboxToken;
+    
     const map = new mapboxgl.Map({
     container: 'mapa', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
+    center: [ -70.59018220682093, -33.59829914646098], // starting position [lng, lat]
+    zoom: 18, // starting zoom
     //projection: 'globe' // display the map as a 3D globe
     });
     map.on('style.load', () => {

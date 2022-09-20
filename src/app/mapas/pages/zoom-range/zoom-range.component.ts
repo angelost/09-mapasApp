424060e088e34120d799a7cb91142de8db59f491
Angelo <angelo.gonzalez@live.com>
2022-09-20 14:40:22 -0300

    312. Controlar el objeto del mapa - ZoomIn ZoomOut

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -23,22 +23,31 @@ import * as mapboxgl from 'mapbox-gl';
     `
   ]
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
+
+  @ViewChild('mapa') divMapa!: ElementRef
+  mapa!: mapboxgl.Map;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     
-    const map = new mapboxgl.Map({
-    container: 'mapa', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [ -70.59018220682093, -33.59829914646098], // starting position [lng, lat]
-    zoom: 18, // starting zoom
-    //projection: 'globe' // display the map as a 3D globe
+    this.mapa = new mapboxgl.Map({
+    container: this.divMapa.nativeElement,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [ -70.59018220682093, -33.59829914646098],
+    zoom: 18,
     });
-    map.on('style.load', () => {
-    map.setFog({}); // Set the default atmosphere style
-    });
+    
+  }
+
+
+  zoomOut() {
+    this.mapa.zoomOut();
+  }
+
+  zoomIn() {
+    this.mapa.zoomIn();
   }
 
 }

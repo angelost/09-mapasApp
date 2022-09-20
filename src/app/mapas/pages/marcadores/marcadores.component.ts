6196155c5e1eff075f0b27359278c9080b63a970
Angelo <angelo.gonzalez@live.com>
2022-09-20 16:13:45 -0300

    317. Marcadores en el mapa

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
+    `
+    .mapa-container {
+      height: 100%;
+      width: 100%;
+    }
+
+    `
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [-70.59018220682093, -33.59829914646098];
 
-  constructor() { }
+  constructor() { }  
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    
+    this.mapa = new mapboxgl.Map({
+    container: this.divMapa.nativeElement,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: this.center,
+    zoom: this.zoomLevel,
+    });
+
+    // const markerHtml: HTMLElement = document.createElement('div');
+    // markerHtml.innerHTML = 'Hola Mundo';
+
+    const maker = new mapboxgl.Marker()
+      .setLngLat( this.center )
+      .addTo( this.mapa );
   }
 
 }
